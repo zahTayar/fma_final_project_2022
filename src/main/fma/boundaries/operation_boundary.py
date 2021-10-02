b1574baf3c32058b8e9fa82f6b96398630041fda
Omer-Malka <omermalka8@gmail.com>
2021-10-02 15:49:51 +0300

    implements operation_boundary, operation_entity and create operation_handler class

---
+++ src/main/fma/boundaries/operation_boundary.py
@@ -0,0 +1,22 @@
+class operation_boundary:
+
+    def __init__(self, operation_id, type, created_timestamp, operation_attributes):
+        self.operation_id = operation_id;
+        self.type = type;
+        self.created_timestamp = created_timestamp;
+        self.operation_attributes = operation_attributes;
+
+    def get_operation_id(self):
+        return self.operation_id;
+
+    def get_type(self):
+        return self.type;
+
+    def get_created_timestamp(self):
+        return self.created_timestamp;
+
+    def get_operation_attributes(self):
+        return self.operation_attributes;
+
+    def set_operation_attributes(self, operation_attributes):
+        self.operation_attributes = operation_attributes;
