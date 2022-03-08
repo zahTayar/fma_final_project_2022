d6909ad5229293866d294a35c4caef857f3db907
zahTayar <zahtayar1993@gmail.com>
2022-03-08 18:43:49 +0200

    delete files

---
+++ src/main/fma/logic/operations/display_relevent_prop.py
@@ -0,0 +1,5 @@
+class display_relevent_prop:
+    def __init__(self):
+        pass
+    def display_relevent_prop(self):
+        print("In display_relevent_prop")
---
+++ src/main/fma/logic/operations/update_db.py
@@ -0,0 +1,6 @@
+class update_db:
+    def __init__(self):
+        pass
+    def update_db(self):
+        print("In update_db")
+
